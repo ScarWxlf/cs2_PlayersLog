3d8048baf90db2d2a316091d200017b9f1930c05
DESKTOP-0B2V58C\HP <kachur-shefer.davyd@student.uzhnu.edu.ua>
2023-11-30 12:05:31 +0200

    😥

--- PlayersLog.cs
+++ PlayersLog.cs
@@ -12,11 +12,6 @@ using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Admin;
 using CounterStrikeSharp.API.Modules.Entities;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using CounterStrikeSharp.API.Modules.Commands;
 using CounterStrikeSharp.API.Modules.Menu;
 using CounterStrikeSharp.API.Modules.Timers;
@@ -40,11 +35,12 @@ public class PlayersLog : BasePlugin
     public HookResult OnPlayerConnect(EventPlayerConnect @event, GameEventInfo info)
     {
         var playerName = @event.Name;
-        var isAdmin = AdminManager.PlayerHasPermissions(@event.Userid, "@css/generic");
+        //var isAdmin = AdminManager.PlayerHasPermissions(@event.Userid, "@css/generic");
         var data = DateTime.Now.ToString("M:MM:yyyy");
         var time = DateTime.Now.ToString("HH:mm:ss");
         var path = "PlayersLog.txt";
-        var text = isAdmin ? $"Admin {playerName} connected, data:[{data}], time:[{time}]" : $"{playerName} connected, data:[{data}], time:[{time}]";
+        //isAdmin ? $"Admin {playerName} connected, data:[{data}], time:[{time}]" : 
+        var text =$"{playerName} connected, data:[{data}], time:[{time}]";
         if (!File.Exists(path))
             File.Create(path).Close();
         File.AppendAllText(path, text + Environment.NewLine);
@@ -55,11 +51,12 @@ public class PlayersLog : BasePlugin
     public HookResult OnPlayerDisconnect(EventPlayerDisconnect @event, GameEventInfo info)
     {
         var playerName = @event.Name;
-        var isAdmin = AdminManager.PlayerHasPermissions(@event.Userid, "@css/generic");
+        //var isAdmin = AdminManager.PlayerHasPermissions(@event.Userid, "@css/generic");
         var data = DateTime.Now.ToString("M:MM:yyyy");
         var time = DateTime.Now.ToString("HH:mm:ss");
         var path = "PlayersLog.txt";
-        var text = isAdmin ? $"Admin {playerName} disconnected, data:[{data}], time:[{time}]" : $"{playerName} disconnected, data:[{data}], time:[{time}]";
+        //isAdmin ? $"Admin {playerName} disconnected, data:[{data}], time:[{time}]" :
+        var text = $"{playerName} disconnected, data:[{data}], time:[{time}]";
         if (!File.Exists(path))
             File.Create(path).Close();
         File.AppendAllText(path, text + Environment.NewLine);

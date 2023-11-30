6d86a580539e8cf21b6ae89a822df35b8b056c6c
DESKTOP-0B2V58C\HP <kachur-shefer.davyd@student.uzhnu.edu.ua>
2023-11-30 11:46:18 +0200

    feat: ura rabotaet

--- PlayersLog.cs
+++ PlayersLog.cs
@@ -12,6 +12,15 @@ using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Admin;
 using CounterStrikeSharp.API.Modules.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Menu;
+using CounterStrikeSharp.API.Modules.Timers;
+using CounterStrikeSharp.API.Modules.Utils;
 
 namespace PlayersLog;
 
@@ -20,17 +29,18 @@ public class PlayersLog : BasePlugin
     public override string ModuleAuthor => "ScarWxlf";
     public override string ModuleName => "PlayersLog";
     public override string ModuleVersion => "1.0.1";
-
+     
     public override void Load(bool hotReload)
     {
-
+        RegisterEventHandler<EventPlayerConnect>(OnPlayerConnect);
+        RegisterEventHandler<EventPlayerDisconnect>(OnPlayerDisconnect);
     }
 
     [GameEventHandler]
-    public HookResult OnPlayerConnect(EventPlayerConnect @event, GameEventInfo info, CCSPlayerController? player)
+    public HookResult OnPlayerConnect(EventPlayerConnect @event, GameEventInfo info)
     {
         var playerName = @event.Name;
-        var isAdmin = AdminManager.PlayerHasPermissions(player, "@css/generic");
+        var isAdmin = AdminManager.PlayerHasPermissions(@event.Userid, "@css/generic");
         var data = DateTime.Now.ToString("M:MM:yyyy");
         var time = DateTime.Now.ToString("HH:mm:ss");
         var path = "PlayersLog.txt";
@@ -42,10 +52,10 @@ public class PlayersLog : BasePlugin
     }
 
     [GameEventHandler]
-    public HookResult OnPlayerDisconnect(EventPlayerConnect @event, GameEventInfo info, CCSPlayerController? player)
+    public HookResult OnPlayerDisconnect(EventPlayerDisconnect @event, GameEventInfo info)
     {
         var playerName = @event.Name;
-        var isAdmin = AdminManager.PlayerHasPermissions(player, "@css/generic");
+        var isAdmin = AdminManager.PlayerHasPermissions(@event.Userid, "@css/generic");
         var data = DateTime.Now.ToString("M:MM:yyyy");
         var time = DateTime.Now.ToString("HH:mm:ss");
         var path = "PlayersLog.txt";
